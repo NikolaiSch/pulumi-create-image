85359b58f9f713e52509ed429f86db51f47590b0
Nikolai Schimke <nikolai@missionctrl.dev>
2024-06-03 15:41:21 +0100

    adding doc comments

--- src/hetzner/virtualMachine.ts
+++ src/hetzner/virtualMachine.ts
@@ -1,6 +1,28 @@
 import * as hcloud from "@pulumi/hcloud";
 import * as pulumi from "@pulumi/pulumi";
 
+/**
+ * HetznerMachineDefinitions describes the configuration options for a new Hetzner VM.
+ *
+ * ## required params
+ * @param serverType The server type to create
+ * @param location The location to create the VM in
+ * @param image The image to use for the VM
+ * @param pubSSHKeys The public SSH keys to use for the VM
+ *
+ * @example
+ * ```ts
+ * let machine = new HetznerVm(
+ * "nixos-vm",
+ * {
+ *  serverType: "cx11",
+ *  location: "nbg1",
+ *  image: "ubuntu-20.04",
+ *  pubSSHKeys: ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC3..."],
+ * },
+ * {})
+ * ```
+ */
 export interface HetznerMachineDefinitions {
   serverType: hcloud.ServerArgs["serverType"];
   location: hcloud.ServerArgs["datacenter"];
@@ -8,6 +30,33 @@ export interface HetznerMachineDefinitions {
   pubSSHKeys: string[];
 }
 
+/**
+ * HetznerVm is a resource component that creates a virtual machine in Hetzner
+ * using the provided machine definitions.
+ *
+ * ## required params
+ * @param name The name of the component
+ * @param cOpts The machine definitions - see {@link HetznerMachineDefinitions}
+ * @param opts The pulumi component options
+ *
+ * ## component outputs
+ * @param ipv4Address - The public IPv4 address of the VM
+ * @param ipv6Address - The public IPv6 address of the VM
+ * @param primaryDiskSize - The size of the primary disk of the VM
+ *
+ * @example
+ * ```ts
+ * let machine = new HetznerVm(
+ * "nixos-vm",
+ * {
+ *  serverType: "cx11",
+ *  location: "nbg1",
+ *  image: "ubuntu-20.04",
+ *  pubSSHKeys: ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC3..."],
+ * },
+ * {})
+ * ```
+ */
 export class HetznerVm extends pulumi.ComponentResource {
   constructor(
     name: string,
